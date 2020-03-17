3dcd01271436d2877186c82b4102d956354fba42
Pides <a_mendespinto@hotmail.com>
2020-03-17 21:50:38 +0000

    copypasterinos
    ok

--- src/sd1920/trab1/server/resources/MessageResource.java
+++ src/sd1920/trab1/server/resources/MessageResource.java
@@ -1,7 +1,55 @@
 package sd1920.trab1.server.resources;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.logging.Logger;
 
-public class MessageResource {
+import javax.inject.Singleton;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response.Status;
+
+import sd1920.trab1.api.Message;
+import sd1920.trab1.api.rest.MessageService;
+import sd1920.trab1.clients.utils.MessageUtills;
+
+@Singleton
+public class MessageResource implements MessageService{
+
+	@Override
+	public long postMessage(String pwd, Message msg) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Message getMessage(String user, long mid, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Message> getMessages(String user, String pwd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeFromUserInbox(String user, long mid, String pwd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteMessage(String user, long mid, String pwd) {
+		// TODO Auto-generated method stub
+		
+	}
 
 	
 }
